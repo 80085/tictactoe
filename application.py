c508cc24033da57bdd91d1fbae1c559e5e54800c
80085 <6232591+80085@users.noreply.github.com>
2020-11-08 15:25:16 +0100

    add readme

--- application.py
+++ application.py
@@ -16,18 +16,11 @@ def train_agent(iterations):
     player_o.save_policy()
 
 
-def single_play(player_symbol):
+def play_game(player_symbol):
     if player_symbol == 'x':
-        play_game(Game(Human('Player One', 'x'), Computer('Player Two', 'o', exp_rate=0)))
+        game = Game(Human('Player One', 'x'), Computer('Player Two', 'o', exp_rate=0))
     else:
-        play_game(Game(Computer('Player One', 'x', exp_rate=0), Human('Player Two', 'o')))
-
-
-def multi_play():
-    play_game(Game(Human('Player One', 'x'), Human('Player Two', 'o')))
-
-
-def play_game(game):
+        game = Game(Computer('Player One', 'x', exp_rate=0), Human('Player Two', 'o'))
     while True:
         print(game.play())
         if input('Play again? (y/n): ').lower() != 'y':
@@ -36,13 +29,11 @@ def play_game(game):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices=['single', 'multi', 'train'])
+    parser.add_argument('mode', choices=['play', 'train'])
     parser.add_argument('--symbol', default='x', choices=['x', 'o'], help='Your symbol. x starts the game')
-    parser.add_argument('--iterations', type=int, default=1000, help='How many games to train')
+    parser.add_argument('--iterations', type=int, default=10000, help='How many games to train')
     args = parser.parse_args()
-    if args.mode == 'single':
-        single_play(args.symbol)
-    elif args.mode == 'multi':
-        multi_play()
+    if args.mode == 'play':
+        play_game(args.symbol)
     elif args.mode == 'train':
         train_agent(args.iterations)
